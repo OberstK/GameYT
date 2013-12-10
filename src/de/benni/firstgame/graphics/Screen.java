bf481058f23c076a32623f7646bc2ef7d0858d56
Benni <Benjamin.Kurczyk@gmail.com>
2013-12-10 16:06:44 +0100

    init

---
+++ src/de/benni/firstgame/graphics/Screen.java
@@ -0,0 +1,41 @@
+package de.benni.firstgame.graphics;
+
+public class Screen {
+
+	private int width;
+	private int height;
+	public int[] pixels;
+
+	int xtime = 100;
+	int ytime = 50;
+	int counter = 0;
+
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+
+	public void clear() {
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+
+	public void render() {
+		counter++;
+		if (counter % 100 == 0) {
+			xtime--;
+		}
+		if (counter % 100 == 0) {
+			ytime--;
+		}
+		for (int y = 0; y < height; y++) {
+			if (ytime < 0 || ytime >= height) break;
+			for (int x = 0; x < width; x++) {
+				if (xtime < 0 || xtime >= width) break;
+				pixels[xtime + ytime * width] = 0xff00ff;
+			}
+		}
+	}
+}
