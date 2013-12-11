79ec602d95a253f42cf846522f95f9e9b113c9fa
Benni <Benjamin.Kurczyk@gmail.com>
2013-12-11 15:25:54 +0100

    Beginning of Entities added

---
+++ src/de/benni/firstgame/entity/Entity.java
@@ -0,0 +1,32 @@
+package de.benni.firstgame.entity;
+
+import java.util.Random;
+
+import de.benni.firstgame.graphics.Screen;
+import de.benni.firstgame.level.Level;
+
+public abstract class Entity {
+
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+
+	public void update() {
+
+	}
+
+	public void render(Screen screen) {
+
+	}
+	
+	public void remove(){
+		//Remove from level
+		removed = true;
+	}
+	
+	public boolean isRemoved(){
+		return removed;
+	}
+
+}
