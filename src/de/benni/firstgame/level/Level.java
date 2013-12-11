a14817d63a64ccc3dc1e9f26f186b9c4c56c298b
Benni <Benjamin.Kurczyk@gmail.com>
2013-12-11 12:54:48 +0100

    Levelsystem added

---
+++ src/de/benni/firstgame/level/Level.java
@@ -0,0 +1,36 @@
+package de.benni.firstgame.level;
+
+import de.benni.firstgame.graphics.Screen;
+
+public class Level {
+
+	protected int width, height;
+	protected int[] tiles;
+
+	public Level(int width, int height) {
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		generateLevel();
+	}
+
+	public Level(String path) {
+		loadLevel(path);
+	}
+
+	protected void generateLevel() {
+
+	}
+
+	public void loadLevel(String path) {
+
+	}
+
+	public void update() {
+
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen){
+		
+	}
+}
